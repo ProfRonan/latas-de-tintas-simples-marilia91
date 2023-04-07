080aada215855434989a567b9ba862c9b3b67e7d
marilia91 <marilia.iza112@gmail.com>
2023-04-07 19:38:26 -0300

    resolução latas de tinta corrigida

--- main.py
+++ main.py
@@ -6,26 +6,21 @@ metros_quadrados = float(metros_quadrados)
 qtd_de_latas = metros_quadrados / 54
 valor_total = qtd_de_latas * 80
 
-if metros_quadrados <= 54: 
-  qtd_de_latas = 1
-  valor_total = 80
-  
-elif metros_quadrados % 54 != 0:
-  qtd_de_latas = int(qtd_de_latas) + 1
-  valor_total = qtd_de_latas * 80
+if metros_quadrados == 0:
+    qtd_de_latas = 0
+    valor_total = 0
+else:
+    if metros_quadrados <= 54: 
+        qtd_de_latas = 1
+        valor_total = 80
 
-else: 
-  qtd_de_latas = metros_quadrados / 54
-  valor_total = qtd_de_latas * 80
-
-#outra solução: 
-#if qtd_de_latas == int(qtd_de_latas):
-    #qtd_de_latas = int(qtd_de_latas)
-
-#else:
-    #qtd_de_latas = int(qtd_de_latas) +  1
-    #valor_total = qtd_de_latas * 80
-    
+    elif metros_quadrados % 54 != 0:
+        qtd_de_latas = int(qtd_de_latas) + 1
+        valor_total = qtd_de_latas * 80
+        
+    else: 
+        qtd_de_latas = metros_quadrados / 54
+        valor_total = qtd_de_latas * 80
 
 # As duas variáveis qtd_de_latas e valor_total devem guardar a resposta do problema
 # Troque os zeros pelos valores corretos.
